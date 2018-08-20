20652956ddaf0c08302c2cb288651e04411ad77c
Glebk0 <glebko123@gmail.com>
2018-08-20 19:07:13 +0300

    scripts

---
+++ dashboard.sh
@@ -0,0 +1,3 @@
+kubectl create -f https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml 
+kubectl create -f https://gist.githubusercontent.com/sbeliakou/e1809c5d479129964c75653c5f9671a0/raw/b8d9fa1c795edacb11bc7204af7ac6058ff06bf7/admin-user.yaml
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')
--- docker_install.sh
+++ docker_install.sh
@@ -17,3 +17,8 @@ cat << EOF > /etc/docker/daemon.json
 "storage-driver": "devicemapper"
 }
 EOF
+systemctl daemon-reload
+systemctl restart docker
+
+join
+  kubeadm join 192.168.1.80:6443 --token jof9ps.x0ume6ukjyfvsjw5 --discovery-token-ca-cert-hash sha256:c085f2cbb127ac37e8b2fe22ba5a46ebf6b489d6f4e9ebc6c09a60aefc0da55e
